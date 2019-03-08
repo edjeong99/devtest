a4e5d748d87f32a1039021680906a6be308272c0
Edward J <edjeong99@gmail.com>
2019-03-08 00:42:54 -0500

    make SOAP API call work

--- dev-lib.php
+++ dev-lib.php
@@ -4,3 +4,32 @@
  * Your code goes here
  * init
  */
+
+ // set environment variable
+putenv("RAZOYO_TEST_KEY=ku%64TeYMo5mAIFj8e");
+
+
+$msg = "Hi Debug Console 2";
+echo $msg;
+
+/*
+
+// import ...  
+
+class SoapClient
+{
+    public function __construct($api_URL) {
+        $this->api_URL = $api_URL;
+        $this->apiUser = $apiUser;
+        $this->apiKey = getenv('RAZOYO_TEST_KEY');
+    }
+
+    public function getDateFromAPI(){
+        $session = $this->login($this->apiUser, $this->apiKey);
+    }
+
+    public function printOut(){
+        echo $this->apiUser;
+    }
+}
+*/
--- export.php
+++ export.php
@@ -16,9 +16,38 @@ $formatKey = 'csv'; // csv, xml, or json
 
 // Connect to SOAP API using PHP's SoapClient class
 // Feel free to create your own classes to organize code
-$soap = new SoapClient($apiWsdl);
-// ...
+// $soap = new SoapClient($apiWsdl);
+// $soap->printOut();
+$contextOptions = array(
+    'ssl' => array(
+    'verify_peer' => false,
+    'verify_peer_name' => false,
+    'allow_self_signed' => true
+    ));
 
+$sslContext = stream_context_create($contextOptions);
+
+$params =  array(
+    'trace' => 1,
+    'exceptions' => true,
+    'cache_wsdl' => WSDL_CACHE_NONE,
+    'stream_context' => $sslContext
+    );
+$client = new SoapClient($apiWsdl, $params);
+
+// If somestuff requires API authentication,
+// then get a session token
+
+$session = $client->login($apiUser, 'ku%64TeYMo5mAIFj8e');
+echo $session;
+
+$result = $client->call($session, 'catalog_product.info', '4');
+echo "result start";
+print_r( $result);
+echo "result end";
+$client->endSession($session);
+// ...
+/*
 // You will need to create a FormatFactory.
 $factory = new FormatFactory(); 
 $format = $factory->create($formatKey);
@@ -27,3 +56,4 @@ $format = $factory->create($formatKey);
 $output = new ProductOutput();
 // ...
 $output->format();
+*/
