2588fbc4cd3c0f74dee417c180f0ac1705f13eb8
Edward J <edjeong99@gmail.com>
2019-03-09 16:40:51 -0500

    making progress of CSVFormat.

--- dev-lib.php
+++ dev-lib.php
@@ -7,19 +7,25 @@
 
 // require('raz-lib.php'); 
 
+
+
  // set environment variable
 putenv("RAZOYO_TEST_KEY=ku%64TeYMo5mAIFj8e");
 
 
-$msg = "Hi Debug Console 2 -- ";
-echo $msg;
-
-
 class CSVFormat implements FormatInterface
 {
+    public $delimiter = ',' ;
+    public $newLine = "\r\n" ;
+
     public function start()
     {
-        echo "Starting";
+     $CSVStart = "Content-type: text/csv \n";
+     $CSVStart .= "Content-Disposition: attachment; filename=productInfo" . date('Y-m-d') . ".csv\r\n"; 
+     $CSVStart .="Pragma: no-cache\n"; 
+     $CSVStart .="Expires: 0 \n"; 
+     $CSVStart .= "SKU" . $this->delimiter . "Name" . $this->delimiter . "Price" . $this->delimiter . "Short Description \r\n";
+     return $CSVStart;
     }
 
     public function finish()
@@ -27,14 +33,45 @@ class CSVFormat implements FormatInterface
         echo "The End";
     }
 
+    // get a product info as argument
+    // return csv format of that product
     public function formatProduct( $product)
     {
-       // process product
-       // return correct format 
-        echo $product['sku'],'\n' ;
+        
+       $outputCSVString ='';
+       $outputArray = array();
+       $cleanedOutputArray = array();
+       $enclosure = '"'; 
+       $delimiter_esc = preg_quote($this->delimiter, '/'); 
+       $enclosure_esc = preg_quote($enclosure, '/');  
+  
+        // get price and short description using product info API
+        $result = $client->call($session, 'catalog_product.info', '4');
+        
+        // SKU, Name, Price, and Short Description
+        $outputArray[0] = $product['sku'];
+        $outputArray[1] = $product['name'];
+        $outputArray[2] = $result['price'];
+        $outputArray[3] = $result['short description'];
+
+        // if content has comma, put double quotes around, so excel won't count that as two separate entries
+        foreach ( $outputArray as $output ) { 
+            if ( preg_match( "/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $output ) ) { 
+              $cleanedOutputArray[] = $enclosure . str_replace($enclosure, $enclosure . $enclosure, $output) . $enclosure; 
+            } else { 
+              $cleanedOutputArray[] = $output; } 
+            } 
+
+        $outputCSVString = implode( $this->delimiter,  $outputArray )."\r\n";
+        return $outputCSVString;
 
     }
 }
+
+// if input is number, convert it to string for csv
+// if (gettype($field) == 'integer' || gettype($field) == 'double') {
+//     $field = strval($field); // Change $field to string if it's a numeric type
+// }
 
 class FormatFactory 
 {
--- export.php
+++ export.php
@@ -42,16 +42,15 @@ $session = $client->login($apiUser, 'ku%64TeYMo5mAIFj8e');
 // get catalog product list from API as array    
 // returns prod id, sku, name
 $apiData = $client->call($session, 'catalog_product.list');
-//$apiData = $client->call($session, 'catalog_product.currentStore', 'english');
 
-echo "result start";
+// echo "result start";
  
- // for test, only get first 2 prodcut from array
+  // for test, only get first 2 prodcut from array
 $apiData = array_chunk($apiData, 2);
 $apiData = $apiData[0];
 
-print_r( $apiData); 
-echo "result end";
+// print_r( $apiData); 
+// echo "result end";
 $client->endSession($session);
 
 // You will need to create a FormatFactory.
@@ -65,5 +64,5 @@ $output = new ProductOutput();
 // set product and format
 $output->setProducts($apiData);
 $output->setFormat($format);
-echo "after setFormat";
+
 $output->format();
